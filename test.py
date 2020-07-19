fbe72b3b8c4f0c2779814262912c3210b85b0f1c
wsm26 <wsm263@gmail.com>
2020-07-20 00:11:22 +0900

    20200720

--- test.py
+++ test.py
@@ -5,44 +5,47 @@ app = Flask(__name__)
 
 @app.route('/')
 def main_page():
-    return render_template('main_page.html')
+    return render_template("main_page.html")
 
-# @app.route('/korea_stock')
-# def korea_stock():
-#     return '국내주식 만드는 곳'
-#
-# @app.route('/korea_stock/<kr_stock>')
-# def get_kr_stock(kr_stock):
-#     return 'profile : ' + kr_stock
-#
-# @app.route('/us_stock')
-# def us_stock():
-#     return '해외주식 만드는 곳'
-#
-# @app.route('/us_stock/<us_stock>')
-# def get_us_stock(us_stock):
-#     return 'profile : ' + us_stock
-#
-# @app.route('/coin')
-# def coin():
-#     return '코인 만드는 곳'
-#
-# @app.route('/coin/<coin_name>')
-# def get_coin(coin_name):
-#     return 'profile : ' + coin_name
-#
-# @app.route('/daily')
-# def daily():
-#     return '매매일지'
-#
-# @app.route('/add_check')
-# def add_check():
-#     return '관심 종목 + 종목 추가'
-#
-# @app.route('/etc')
-# def etc():
-#     return '기타'
+@app.route('/korea_stock')
+def korea_stock():
+    return render_template('korea_stock.html')
 
+@app.route('/korea_stock/<kr_stock>')
+def get_kr_stock(kr_stock):
+    return 'profile : '+kr_stock
+
+@app.route('/us_stock')
+def us_stock():
+    return render_template('us_stock.html')
+
+@app.route('/us_stock/<us_stock>')
+def get_us_stock(us_stock):
+    return 'profile : ' + us_stock
+
+@app.route('/coin')
+def coin():
+    return render_template('us_stock.html')
+
+@app.route('/coin/<coin_name>')
+def get_coin(coin_name):
+    return 'profile : ' + coin_name
+
+@app.route('/sales_log')
+def sales_log():
+    return render_template('sales_log.html')
+
+@app.route('/add_check')
+def add_check():
+    return render_template('add_check.html')
+
+@app.route('/twit') # 게시판 : 추후 타임라인처럼 해보기
+def twit():
+    return render_template('twit.html')
+
+@app.route('/etc')
+def etc():
+    return render_template('etc.html')
 
 if __name__ == '__main__':
     app.run()
---
+++ 크롤링.py
@@ -0,0 +1,98 @@
+# 네이버 국내 증시 메인 페이지
+
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://finance.naver.com/"
+
+index1 = requests.get(URL)
+html = index1.text
+soup = BeautifulSoup(html, 'html.parser')
+korea_index_information = soup.find('div',class_='section_stock_market')
+us_index_information = soup.find('div',class_='group_aside')
+market_index_information = soup.find('div',class_='article2')
+
+# ##### 1. 한국 인덱스
+# # 코스피
+# kospi = korea_index_information.find('div',class_='kospi_area group_quot quot_opn').text.split(' ')
+# kospi_close = kospi[2]
+# kospi_change = kospi[3]
+# kospi_change_rate = kospi[4]
+# kospi_ant = kospi[9].split('\n')[9]
+# kospi_foreigner = kospi[9].split('\n')[16]
+# kospi_ins = kospi[9].split('\n')[23]
+# print('코스피 현재가 : ', kospi_close)
+# print('코스피 변화 : ', kospi_change)
+# print('코스피 변화율 : ', kospi_change_rate)
+# print('코스피 개인: ', kospi_ant)
+# print('코스피 외국인: ', kospi_foreigner)
+# print('코스피 기관: ', kospi_ins)
+#
+#
+# # 코스닥
+# kosdaq = korea_index_information.find('div',class_='kosdaq_area group_quot').text.split(' ')
+# kosdaq_close = kosdaq[2]
+# kosdaq_change = kosdaq[3]
+# kosdaq_change_rate = kosdaq[4]
+# kosdaq_ant = kosdaq[9].split('\n')[9]
+# kosdaq_foreigner = kosdaq[9].split('\n')[16]
+# kosdaq_ins = kosdaq[9].split('\n')[23]
+# print('코스닥 현재가 : ', kosdaq_close)
+# print('코스닥 변화 : ', kosdaq_change)
+# print('코스닥 변화율 : ', kosdaq_change_rate)
+# print('코스닥 개인: ', kosdaq_ant)
+# print('코스닥 외국인: ', kosdaq_foreigner)
+# print('코스닥 기관: ', kosdaq_ins)
+#
+# # 코스피200
+# kospi200 = korea_index_information.find('div',class_='kospi200_area group_quot').text.split(' ')
+# kospi200_close = kospi200[2]
+# kospi200_change = kospi200[3]
+# kospi200_change_rate = kospi200[4]
+# print('코스피200 현재가 : ', kospi200_close)
+# print('코스피200 변화 : ', kospi200_change)
+# print('코스피200 변화율 : ', kospi200_change_rate)
+#
+#
+#
+# ##### 2. 미국 인덱스
+# us_index = us_index_information.find('tbody').text.split(' ')
+# nasdaq = us_index[2:4]
+# nasdaq_close = nasdaq[0].split('\n')[1]
+# nasdaq_change = [nasdaq[0].split('\n')[2],nasdaq[1].split('\n')[0]]
+# print('나스닥 현재가 : ', nasdaq_close)
+# print('나스닥 변화 : ', nasdaq_change)
+#
+#
+# # 2. 다우 산업
+# dow = us_index[0:2]
+# dow_close = dow[0].split('\n')[3]
+# dow_change = [dow[0].split('\n')[4],dow[1].split('\n')[0]]
+# print('다우산업 현재가 : ', dow_close)
+# print('다우산업 변화 : ', dow_change)
+
+# ##### 기타 인덱스
+# exchange = market_index_information.find('tbody')
+# # 1. 달러
+# usd = exchange.find_all('td')[0].text
+# usd_rate = exchange.find_all('td')[1].text.split(' ')
+# print(usd)
+# print(usd_rate)
+# # 2. 유로
+# eur = exchange.find_all('td')[2].text
+# eur_rate = exchange.find_all('td')[3].text.split(' ')
+# print(eur)
+# print(eur_rate)
+# # 3. 엔화
+# jpy = exchange.find_all('td')[4].text
+# jpy_rate = exchange.find_all('td')[5].text.split(' ')
+# print(jpy)
+# print(jpy_rate)
+market = market_index_information.find_all('div',class_='group2')
+# 4. WTI
+
+# 5. 금
+여기서 부터 다시하기
+# 6. 달러인덱스
+print(market)
+# print(market.find('tr', class_="up bold"))
