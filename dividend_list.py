7738289566cc6e438ae2dbcf0f514adf59dd2092
sumin <wsm263@gmail.com>
2020-07-21 18:53:30 +0900

    20200721

--- dividend_list.py
+++ dividend_list.py
@@ -1,12 +1,37 @@
-# import requests
-# from bs4 import BeautifulSoup
-#
-URL = "https://www.dividend.com/dividend-stocks/high-dividend-yield-stocks/#tm=3-high-yield-stocks&r=Webpage%231281&f_35=true&f_9_min=2&f_9_max=100&only=meta%2Cdata%2Cthead&"\
-      +"page=1"+"&sort_by=latest_yield&sort_direction=desc"
-#
-# index1 = requests.get(URL)
-# html = index1.text
-# soup = BeautifulSoup(html, 'html.parser')
-# test = soup.find('section', class_='tm').find('tbody').find_all('td')
-#
-# print([test[i].text for i in range(1,181,9)])
+import pandas as pd
+import requests
+from bs4 import BeautifulSoup
+
+URL = "https://www.slickcharts.com/sp500"
+
+index1 = requests.get(URL)
+html = index1.text
+soup = BeautifulSoup(html, 'html.parser')
+test = soup.find('div', class_='table-responsive').text.split('\n')
+
+
+inc_name, inc_code, price, change, change_per = [],[],[],[],[]
+for i in range(15,4507,9):
+    inc_name.append(test[i+1])
+    inc_code.append(test[i+2])
+    price.append(test[i+4])
+    change.append(test[i+5])
+    change_per.append(test[i+6])
+
+movements = []
+for i in change:
+    if float(i)>0:
+        movements.append('상승')
+    elif float(i) == 0:
+        movements.append('보합')
+    else:
+        movements.append('하락')
+
+
+data = [inc_name, inc_code, price, change, change_per, movements]
+
+df = pd.DataFrame(data).T
+df.columns = ['inc_name', 'inc_code', 'price', 'change', 'change_per', 'movements']
+print(df)
+df.to_csv('s&p500.csv')
+
