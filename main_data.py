2f8ff6faa5e4e7b199a6cd2bc819490db569cb6b
sumin <wsm263@gmail.com>
2020-07-21 14:33:21 +0900

    20200721

--- main_data.py
+++ main_data.py
@@ -107,3 +107,16 @@ def main_page_value():
               change_value_list[4], gold, change_value_list[5]]
 
     return values
+# # 재무제표 부분
+#
+# import requests
+# from bs4 import BeautifulSoup
+#
+# URL = "https://finance.naver.com/sise/sise_index.nhn?code=KOSPI"
+#
+# index1 = requests.get(URL)
+# html = index1.text
+# soup = BeautifulSoup(html, 'html.parser')
+# keywords = soup.find_all('div',class_='subtop_sise_detail')
+#
+# print(keywords)
