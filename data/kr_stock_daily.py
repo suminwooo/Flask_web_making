67f5546f74c3411163fc1cd02f5d2a038ae27c19
wsm26 <wsm263@gmail.com>
2020-08-09 14:20:38 +0900

    20200809

---
+++ data/kr_stock_daily.py
@@ -0,0 +1,48 @@
+from etc.date import date_num
+import pandas as pd
+import FinanceDataReader as fdr
+
+data = pd.read_csv('C:/Users/wsm26/Desktop/Flask_web_making/crolling_n_data/raw_data/kospi_code.csv')
+
+code_list = []
+for i in data['code']:
+    code = str(i)
+    if len(code) == 2:
+        code = '0000' + code
+        code_list.append(code)
+    elif len(code) == 3:
+        code = '000' + code
+        code_list.append(code)
+    elif len(code) == 4:
+        code = '00' + code
+        code_list.append(code)
+    elif len(code) == 5:
+        code = '0' + code
+        code_list.append(code)
+    else:
+        code_list.append(code)
+
+class kr_stock_daily:
+
+    def daily_data(self):
+        dataframe_test = pd.DataFrame()
+
+        for code in code_list:
+            df = fdr.DataReader(code, date_num())
+            df = df.reset_index()
+            df.columns = ['date', 'close', 'open', 'high', 'low', 'volume', 'close_diff']
+            df['code'] = code
+            df = df[['code', 'date', 'close', 'close_diff', 'open', 'high', 'low', 'volume']]
+            dataframe_test = pd.concat([dataframe_test, df], axis=0)
+
+        final_data = dataframe_test.dropna(axis=0).reset_index()[
+            ['code', 'close', 'close_diff', 'open', 'high', 'low', 'volume']]
+        final_data.columns = ['kr_stock_code', 'kr_stock_close', 'kr_stock_close_diff', 'kr_stock_open',
+                              'kr_stock_high', 'kr_stock_low', 'kr_stock_volume']
+
+        final_data['date'] = date_num()
+        final_data = final_data[
+            ['kr_stock_code', 'date', 'kr_stock_close', 'kr_stock_close_diff', 'kr_stock_open', 'kr_stock_high',
+             'kr_stock_low', 'kr_stock_volume']]
+
+        return final_data
---
+++ data/us_stock_weekly.py
@@ -0,0 +1,41 @@
+from etc.date import date_num
+import yfinance as yf
+import pandas as pd
+
+data = pd.read_csv('C:/Users/wsm26/Desktop/Flask_web_making/crolling_n_data/raw_data/us_stock_list.csv')
+stock_name = list(data['us_stock_code'])
+
+
+class us_stock_weekly:
+    def weekly_data(self):
+
+        final_list = []
+        for code in stock_name:
+            test = yf.Ticker(code)
+            value_list= []
+            value_list.append(code)
+            value_list.append(date_num())
+            for i in ['regularMarketOpen','twoHundredDayAverage','payoutRatio','regularMarketDayHigh','averageDailyVolume10Day','totalAssets',
+                          'regularMarketPreviousClose','trailingAnnualDividendRate','averageVolume10days','dividendRate','exDividendDate','beta',
+                          'regularMarketDayLow','regularMarketVolume','averageVolume','fiftyTwoWeekHigh','fiveYearAvgDividendYield','fiftyTwoWeekLow',
+                          'bid','dividendYield','lastDividendValue','enterpriseValue','threeYearAverageReturn','fiveYearAverageReturn',
+                          'regularMarketPrice']:
+                try:
+                    value_list.append(test.info[i])
+                except:
+                    value_list.append('NO_DATA')
+            final_list.append(value_list)
+
+        data = pd.DataFrame(columns = ['code','date','regularMarketOpen','twoHundredDayAverage','payoutRatio','regularMarketDayHigh','averageDailyVolume10Day','totalAssets',
+                          'regularMarketPreviousClose','trailingAnnualDividendRate','averageVolume10days','dividendRate','exDividendDate','beta',
+                          'regularMarketDayLow','regularMarketVolume','averageVolume','fiftyTwoWeekHigh','fiveYearAvgDividendYield','fiftyTwoWeekLow',
+                          'bid','dividendYield','lastDividendValue','enterpriseValue','threeYearAverageReturn','fiveYearAverageReturn',
+                          'regularMarketPrice'])
+        for i in range(len(final_list)):
+            if len(final_list[i]) == 27:
+                data.loc[i] = final_list[i]
+            else:
+                new_value = final_list[i]+['NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA','NO_DATA']
+                data.loc[i] = new_value
+
+        return data
